0c35db9be5a92cbccfe799ae25f419b8e720ebdf
Kris Dudkiewicz <doodeck@users.noreply.github.com>
2015-01-04 21:24:59 +0100

    Added dummy Lambda "execution environment"

---
+++ cacherandom/cacherandom_test.js
@@ -0,0 +1,19 @@
+// cacherandom_test.js
+/*
+This dummy Lambda "execution environment" is very handy
+ in finding the syntactic error, which otherwise are usually
+ not reported by the actual framework.
+ */
+var cacherandom = require('./cacherandom');
+
+var event = {
+  "rmIds": [ "519", 520 ]
+};
+
+var context = {
+  done: function() {
+    console.log('context.done called: ', JSON.stringify(arguments));
+  }
+}
+cacherandom.handler(event, context);
+
