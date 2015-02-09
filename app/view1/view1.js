10513d9fee3aab3b989b048024051147598ff482
Kris Dudkiewicz <doodeck@users.noreply.github.com>
2015-02-09 07:13:35 +0100

    Added UI for numbers and extras, not working yet

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -31,8 +31,21 @@ angular.module('myApp.view1', ['ngRoute'])
                   function($scope,   $http,   AWSService,   LastEvalKey,   Country) {
 	$scope.pickCounts = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 50 ];
   $scope.currentCount = $scope.pickCounts[0];
+  $scope.pickNumbers = [1,2,3,4,5,6,7,8,9,10,12,15,16,17,18];
+  $scope.currentNumber = $scope.pickNumbers[0];
+  $scope.pickHighNumbers = [15,19,20,22,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,
+                            40,41,42,43,44,45,46,47,48,49,50,52,53,54,55,56,58,59,60,70,
+                            75,77,80,81,90];
+  $scope.currentHighNumber = $scope.pickHighNumbers[0];
+  $scope.pickExtras = [0,1,2,4,5,6,7,10];
+  $scope.currentExtra = $scope.pickExtras[0];
+  $scope.pickHighExtras = [0,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,
+                           25,26,27,31,35,39,42,43,45,46,49,52,70,80];
+  $scope.currentHighExtra = $scope.pickHighExtras[0];
+
   $scope.countries = Country.query();
   $scope.currentCountry = $scope.countries[1];
+
   $scope.viewName = "view 1";
   $scope.progress = {
     max: 100, // it cannot be changed dynamically
