915da1c5ba4d1e6cbb7d8774553c5664085b0de9
Kris Dudkiewicz <doodeck@users.noreply.github.com>
2015-02-25 07:01:19 +0100

    CLI deployment script

---
+++ cacherandom/upload-function.sh
@@ -0,0 +1,3 @@
+#
+
+zip -r tmp/index.zip *.js modules/ node_modules/ ; aws --profile lambda lambda upload-function --region eu-west-1 --function-name cacherandom --function-zip tmp/index.zip  --role 'arn:aws:iam::915133436062:role/lotto_lambda_exec_role' --mode event --handler cacherandom.handler --runtime nodejs --timeout 10
