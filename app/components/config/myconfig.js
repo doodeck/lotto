2dd1465206870393f4a74a155ba5e055f0788c7d
Kris Dudkiewicz <doodeck@users.noreply.github.com>
2015-04-06 15:56:06 +0200

    With global unique functionality the duration of the process may be londer

--- app/components/config/myconfig.js
+++ app/components/config/myconfig.js
@@ -12,5 +12,5 @@ angular.module('myApp.config', [])
     'IdentityPoolId': 'eu-west-1:74afdbda-6525-498f-b13a-b7d940aac2bd',
     'RoleArn': 'arn:aws:iam::915133436062:role/Cognito_LambdaRandomUnauth_DefaultRole'    
   },
-  'GUItimeout': '10000' // milliseconds
+  'GUItimeout': '20000' // milliseconds
 })
