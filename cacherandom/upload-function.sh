a2aab550f16c33660118bc30bd366a7c91cec1dd
Kris Dudkiewicz <doodeck@users.noreply.github.com>
2015-06-04 19:08:48 +0200

    Added mkdir

--- cacherandom/upload-function.sh
+++ cacherandom/upload-function.sh
@@ -1,5 +1,6 @@
 #
 
+mkdir -p tmp
 zip -r tmp/index.zip *.js modules/ node_modules/
 # As of CLI 1.7 upload-function option has been discontinued. Use update-function-code instead
 # aws --profile lambda lambda upload-function --region eu-west-1 --function-name cacherandom --function-zip tmp/index.zip  --role 'arn:aws:iam::915133436062:role/lotto_lambda_exec_role' --mode event --handler cacherandom.handler --runtime nodejs --timeout 10
